ff52e92dcd4bb3ed68ee7098efaa724f38282181
方楚星 <50919312+fcxfcx@users.noreply.github.com>
2023-03-15 16:17:21 +0800

    补充socket注释，开始添加session注释

--- alvr/sockets/src/control_socket.rs
+++ alvr/sockets/src/control_socket.rs
@@ -10,6 +10,7 @@ use std::{marker::PhantomData, net::IpAddr};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_util::codec::Framed;
 
+// 控制信息的socket和数据流socket相似，不过只用TCP
 pub struct ControlSocketSender<T> {
     inner: SplitSink<Framed<TcpStream, Ldc>, Bytes>,
     _phantom: PhantomData<T>,
@@ -17,6 +18,7 @@ pub struct ControlSocketSender<T> {
 
 impl<S: Serialize> ControlSocketSender<S> {
     pub async fn send(&mut self, packet: &S) -> StrResult {
+        // 发送是通过序列化的方式发送数据的
         let packet_bytes = bincode::serialize(packet).map_err(err!())?;
         self.inner.send(packet_bytes.into()).await.map_err(err!())
     }
@@ -29,6 +31,7 @@ pub struct ControlSocketReceiver<T> {
 
 impl<R: DeserializeOwned> ControlSocketReceiver<R> {
     pub async fn recv(&mut self) -> StrResult<R> {
+        // 接收数据并反序列化
         let packet_bytes = self
             .inner
             .next()
@@ -57,8 +60,10 @@ pub enum PeerType<'a> {
 }
 
 impl ProtoControlSocket {
+    // 注意，这里的客户端仅指的是发起连接的一方，并不是ALVR的client，因为在ALVR里，client是接受server发起的连接的一方
     pub async fn connect_to(peer: PeerType<'_>) -> StrResult<(Self, IpAddr)> {
         let socket = match peer {
+            // 作为发起连接的一方
             PeerType::AnyClient(ips) => {
                 let client_addresses = ips
                     .iter()
@@ -68,6 +73,7 @@ impl ProtoControlSocket {
                     .await
                     .map_err(err!())?
             }
+            // 作为接受连接的一方
             PeerType::Server(listener) => {
                 let (socket, _) = listener.accept().await.map_err(err!())?;
                 socket
@@ -76,17 +82,20 @@ impl ProtoControlSocket {
 
         socket.set_nodelay(true).map_err(err!())?;
         let peer_ip = socket.peer_addr().map_err(err!())?.ip();
+        // 还是一样的利用Framed让socket可以发送和接收固定长度的数据
         let socket = Framed::new(socket, Ldc::new());
 
         Ok((Self { inner: socket }, peer_ip))
     }
 
     pub async fn send<S: Serialize>(&mut self, packet: &S) -> StrResult {
+        // 序列化，发送数据
         let packet_bytes = bincode::serialize(packet).map_err(err!())?;
         self.inner.send(packet_bytes.into()).await.map_err(err!())
     }
 
     pub async fn recv<R: DeserializeOwned>(&mut self) -> StrResult<R> {
+        // 用next方法接收数据，然后反序列化
         let packet_bytes = self
             .inner
             .next()
@@ -99,6 +108,7 @@ impl ProtoControlSocket {
     pub fn split<S: Serialize, R: DeserializeOwned>(
         self,
     ) -> (ControlSocketSender<S>, ControlSocketReceiver<R>) {
+        // 分割为发送和接收两个socket，Sink是发送的，Stream是接收的
         let (sender, receiver) = self.inner.split();
 
         (
